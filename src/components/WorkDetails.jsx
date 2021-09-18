5a636daadae8af9ce4cbe47f625e99773fde9c21
KeithCheongWaiKeong <keithcheongwaikeong@gmail.com>
2021-09-18 18:10:20 +0800

    Bug Fix: Removed pdf feature

--- src/components/WorkDetails.jsx
+++ src/components/WorkDetails.jsx
@@ -44,7 +44,7 @@ const WorkDetails = (props) => {
           <img className={classes.logo} src={logo} alt="" />
         </Grid>
       )}
-      {false ||
+      {false &&
         (pdf && (
           <Grid className={classes.contentContainer} item lg={12} md={12} sm={12} xs={12}>
             <Typography id="pdf-title" className={classes.contentTitle}>
